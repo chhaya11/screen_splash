5fa9bc3666299163a624a8daa9cb0488470d9122
chhaya <schhaya1997@gmail.com>
2022-05-10 18:32:04 +0530

    code of show product detail

--- lib/scan_screen.dart
+++ lib/scan_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:ui';
+
 import 'package:another_flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:mobile_scanner/mobile_scanner.dart';
@@ -13,6 +16,8 @@ class Scan_Screen extends StatefulWidget {
 class _Scan_ScreenState extends State<Scan_Screen> {
   @override
   MobileScannerController cameraController = MobileScannerController();
+  String barcodeAssign = "";
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
@@ -21,25 +26,13 @@ class _Scan_ScreenState extends State<Scan_Screen> {
               allowDuplicates: false,
               controller: cameraController,
               onDetect: (barcode, args) {
-                 final String? code = barcode.rawValue;
-                if (code?.isEmpty==false) {
-                    showDialog(context: context, builder: (context) {
-                        return AlertDialog(
-                          
-                          content: Text("Hello"),
-                          elevation: 0,
-                          backgroundColor: Colors.transparent,
-                        );
-                      }, barrierColor: Colors.transparent);
-                    
-                  // showFlushbar(
-                  //     'Successful Scan',
-                  //     'You can now view the same in history',
-                  //     Icon(Icons.check_circle_outline_rounded,
-                  //         color: Colors.red),
-                  //     context);
-                } else {
-                  showFlushbar('hye', 'bye', Icon(Icons.boy), context);
+                barcodeAssign = barcode.rawValue ?? "";
+                if (barcodeAssign.isEmpty == false) {
+                  Timer(Duration(seconds: 3), () {
+                    Navigator.pop(context);
+                    showProductDetailDialog();
+                  });
+                  showSuccessDialog();
                 }
                 // print('Barcode found! $code');
               }),
@@ -96,5 +89,165 @@ class _Scan_ScreenState extends State<Scan_Screen> {
         ],
       ),
     );
+  }
+
+  void showSuccessDialog() {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            contentPadding: const EdgeInsets.all(0),
+            content: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Expanded(
+                    flex: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(20),
+                            bottomLeft: Radius.circular(20)),
+                        color: Colors.green,
+                      ),
+                      child: Icon(
+                        Icons.check_circle_outline_rounded,
+                        color: Colors.white,
+                        size: 40.0,
+                      ),
+                      height: 100,
+                    )),
+                SizedBox(
+                  width: 15,
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Successful Scan",
+                        style: TextStyle(color: Colors.black, fontSize: 20.0),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        'You can now view the same in history',
+                        style: TextStyle(color: Colors.grey, fontSize: 12.0),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)),
+            elevation: 0,
+          );
+        },
+        barrierColor: Colors.transparent);
+  }
+
+  void showProductDetailDialog() {
+    showDialog(
+        context: context,        
+        builder: (context) {
+          return Container(
+            height: 200,
+            child: Stack(             
+              children:[
+                Positioned.fill(
+                child: AlertDialog(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0)),
+                  content: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [                      
+                      Text(
+                        'Product Details',
+                        style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Image.asset('asset/images/qr-code-detail.png'),
+                      SizedBox(
+                        height: 20.0,
+                      ),
+                      Text('ID - ${barcodeAssign}',
+                          style: TextStyle(
+                              color: Colors.black, fontWeight: FontWeight.w600),
+                          textAlign: TextAlign.center),
+                      SizedBox(
+                        height: 10.0,
+                      ),
+                      Text(
+                        'product : pamper',
+                        style: TextStyle(color: Colors.grey, fontSize: 12.0),
+                      ),
+                      SizedBox(
+                        height: 30.0,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Company Name",
+                            style: TextStyle(color: Colors.grey, fontSize: 13.0),
+                          ),
+                          Text(
+                            "CheckMyProduct",
+                            style: TextStyle(color: Colors.black, fontSize: 14.0),
+                          )
+                        ],
+                      ),
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Quantity",
+                            style: TextStyle(color: Colors.grey, fontSize: 13.0),
+                          ),
+                          Text(
+                            "6 Pec",
+                            style: TextStyle(color: Colors.black, fontSize: 14.0),
+                          )
+                        ],
+                      ),                
+                      Divider(),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Text(
+                            "Price",
+                            style: TextStyle(color: Colors.grey, fontSize: 13.0),
+                          ),
+                          Text(
+                            "2.65",
+                            style: TextStyle(color: Colors.black, fontSize: 14.0),
+                          )
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            Positioned(
+              right: 0,
+              child: Container(
+                child:Image.asset('asset/images/cross.png'),
+              ),
+            ),
+              ]
+            ),
+          );
+        });
   }
 }
